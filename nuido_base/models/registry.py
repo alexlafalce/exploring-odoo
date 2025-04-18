a9b5cf94c8e57e4580e5ce925da8c758e95a1020
Yoni <yoni.tjio@gmail.com>
2025-04-18 18:25:35 +0700

    Add nuido_base

---
+++ nuido_base/models/registry.py
@@ -0,0 +1,21 @@
+# THIS FILE IS A PART OF PUBLIC REPOSITORY: https://github.com/yonitjio/exploring-odoo
+# THIS SOFTWARE IS RELEASED UNDER THE MIT LICENSE: https://opensource.org/licenses/MIT
+# THIS SOFTWARE IS EXPERIMENTAL AND FOR EDUCATIONAL PURPOSE ONLY. DO NOT USE IT IN PRODUCTION.
+
+import logging
+_logger = logging.getLogger(__name__)
+
+from odoo import models, fields
+
+class NuidoBaseRegistry(models.Model):
+    _name = "nuido_base.registry"
+    _description = "Nuido Registry"
+    _rec_name = "key"
+
+    category = fields.Char("Category", required=True)
+    key = fields.Char("Key", required=True)
+    value = fields.Char("Value", required=True)
+
+    _sql_constraints = [
+        ('category_key_uniq', 'unique (category, key)', "Key already exists in category!"),
+    ]
---
+++ nuido_base/static/src/components/sections/dropdown_section.js
@@ -0,0 +1,27 @@
+// THIS FILE IS A PART OF PUBLIC REPOSITORY: https://github.com/yonitjio/exploring-odoo
+// THIS SOFTWARE IS RELEASED UNDER THE MIT LICENSE: https://opensource.org/licenses/MIT
+// THIS SOFTWARE IS EXPERIMENTAL AND FOR EDUCATIONAL PURPOSE ONLY. DO NOT USE IT IN PRODUCTION.
+
+import { useState } from "@odoo/owl";
+import { makeReactive } from "@nuido/utils/utils";
+import { NodeSection } from "@nuido/components/node_section";
+import { NodeSectionWithRoleModel } from "@nuido_base/models/sections/node_section_with_role";
+export class DropdownSectionModel extends NodeSectionWithRoleModel {
+    constructor() {
+        super(...arguments);
+        this.label = "Input";
+        this.options = [];
+    }
+}
+export class DropdownSection extends NodeSection {
+    setup() {
+        super.setup();
+        const state = {
+            value: this.props.section.value || this.props.section.default || ""
+        };
+        this.state = useState(makeReactive(this, state, (owner, data) => {
+            this.props.section.value = data.value;
+        }));
+    }
+}
+DropdownSection.template = "nuido_base.dropdown-input-section";
